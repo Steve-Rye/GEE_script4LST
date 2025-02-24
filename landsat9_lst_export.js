b21da91c699f10095c9b7141eacf92572e195b18
Steve-Rye <3205355172@qq.com>
2025-02-24 15:13:23 +0800

    refactor(landsat): 重构地表温度计算和图像导出脚本
    - 更新了 Landsat 5、7 和 9 的脚本，统一了代码结构
    - 改进了地表温度计算方法，提高了代码可读性
    - 添加了真彩 RGB 图层显示功能
    - 优化了图像导出流程，增加了元信息打印
    - 调整了时间范围和其他参数，以适应不同的数据集

--- landsat9_lst_export.js
+++ landsat9_lst_export.js
@@ -1,53 +1,61 @@
-// 时间范围，可修改
+// 设置时间范围
 var startDate = '2022-09-01';
 var endDate = '2022-10-01';
 
-// 条带号，可修改
+// 设置条带号
 var path = 122;
 var row = 44;
 
 // 加载 Landsat 9 数据集（Landsat 9 Level 2, Collection 2, Tier 1数据集）
 var dataset = ee.ImageCollection('LANDSAT/LC09/C02/T1_L2')
     .filterDate(startDate, endDate) // 过滤时间
-    .filter(ee.Filter.eq('WRS_PATH', path)) // 过滤path
-    .filter(ee.Filter.eq('WRS_ROW', row));  // 过滤row
+    .filter(ee.Filter.eq('WRS_PATH', path)) // 过滤 Path
+    .filter(ee.Filter.eq('WRS_ROW', row));  // 过滤 Row
 
-// 应用比例因子和偏移量校正 ST_B10 波段
+// 应用比例因子和偏移量校正，计算地表温度（LST）
 var applyScaleFactors = function(image) {
-  var thermalBands = image.select('ST_B10') // 选择ST_B10波段
-                          .multiply(0.00341802) // 乘以比例因子
-                          .add(149.0).subtract(273.15); // 加上偏移量并转换为摄氏度
-  return image.addBands(thermalBands.rename('LST'), null, true); // 重命名为LST
+  var LST = image.select('ST_B10')
+                  .multiply(0.00341802)
+                  .add(149.0).subtract(273.15); // 转换为摄氏度
+  return image.addBands(LST.rename('LST'), null, true);
 };
 
 dataset = dataset.map(applyScaleFactors);
 
-// 地图中心定位
+// 地图中心定位到数据集
 Map.centerObject(dataset);
 
-// 导出图像
+// 导出 LST 图像
 var exportImages = function(image) {
   var fileName = image.get('LANDSAT_PRODUCT_ID');
 
   Export.image.toDrive({
-    image: image.select('LST'), // 选择LST波段
+    image: image.select('LST'),
     description: fileName.getInfo(),
-    scale: 30, // 分辨率
+    scale: 30,
     region: image.geometry(),
     maxPixels: 1e13
   });
 };
 
-// 循环遍历并导出
+// 循环遍历并导出图像
 var imageList = dataset.toList(dataset.size());
+
+// 添加真彩RGB图层
+var addLayer = function(image, name) {
+  Map.addLayer(image, {bands: ['SR_B4', 'SR_B3', 'SR_B2'], min: 0, max: 65535, gamma: 2.0}, name);
+};
+
+var evaluateFileName = function(image) {
+  var fileName = ee.String(image.get('LANDSAT_PRODUCT_ID'));
+  fileName.evaluate(function(name) {
+    addLayer(image, name);
+  });
+};
 
 for (var i = 0; i < imageList.size().getInfo(); i++) {
   var image = ee.Image(imageList.get(i));
-  
-  // LST波段显示参数
-  var lstParams = {min: 0, max: 60, palette: ['black', 'white']};
-  // 添加LST图层
-  Map.addLayer(image.select('LST'), lstParams, image.get('LANDSAT_PRODUCT_ID').getInfo() + '_LST');
-  
+  evaluateFileName(image);
+  print(image.getInfo()); // 打印图像元信息
   exportImages(image);
 }
